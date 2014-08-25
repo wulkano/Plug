67a773c8e4eb0c4c140661182c10ef09ddb85816
Alex Marchant <alexjmarchant@gmail.com>
2014-08-25 01:27:47 -0400

    Basic login window framing

---
+++ Plug/LoginButtonCell.swift
@@ -0,0 +1,13 @@
+//
+//  LoginButtonCell.swift
+//  Plug
+//
+//  Created by Alex Marchant on 8/25/14.
+//  Copyright (c) 2014 Plug. All rights reserved.
+//
+
+import Cocoa
+
+class LoginButtonCell: NSButtonCell {
+
+}
---
+++ Plug/RoundedRectOutlineButton.swift
@@ -0,0 +1,26 @@
+//
+//  RoundedRectOutlineButton.swift
+//  Plug
+//
+//  Created by Alex Marchant on 8/25/14.
+//  Copyright (c) 2014 Plug. All rights reserved.
+//
+
+import Cocoa
+
+class RoundedRectOutlineButton: NSButton {
+    @IBInspectable var radius: CGFloat = 3
+    @IBInspectable var strokeWidth: CGFloat = 1
+    @IBInspectable var strokeColor: NSColor = NSColor.whiteColor()
+
+    override func drawRect(dirtyRect: NSRect) {
+        
+        super.drawRect(dirtyRect)
+        
+        var rect = NSMakeRect(0.5, 0.5, bounds.size.width - 1, bounds.size.height - 1)
+        var roundedRect = NSBezierPath(roundedRect: rect, xRadius: radius, yRadius: radius)
+        roundedRect.lineWidth = strokeWidth
+        strokeColor.set()
+        roundedRect.stroke()
+    }
+}
