66b29c5e459053bf5090f8af65099d772a25b14a
Alex Marchant <alexjmarchant@gmail.com>
2015-05-09 22:12:15 -0400

    stash

--- Plug/Plug-Bridging-Header.h
+++ Plug/Plug-Bridging-Header.h
@@ -2,10 +2,10 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <INAppStoreWindow.h>
-#import <AFNetworking.h>
-#import <SSKeychain.h>
-#import <CommonCrypto/CommonCrypto.h>
+#import "INAppStoreWindow.h"
+#import "AFNetworking.h"
+#import "SSKeychain.h"
+#import "CommonCrypto/CommonCrypto.h"
 #import "ITSwitch.h"
 #import "SLColorArt.h"
 #import "NSObject+SPInvocationGrabbing.h"
