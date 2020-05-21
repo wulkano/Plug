fa51e020f3eb1df861c819b338eeafc8622f2387
Sindre Sorhus <sindresorhus@gmail.com>
2020-05-22 01:04:39 +0800

    Disable analytics

--- Plug/Analytics.swift
+++ Plug/Analytics.swift
@@ -1,21 +1,23 @@
 import Cocoa
 
+// TODO: Decide what to do about analytics. It should at least be opt-in.
+
 enum Analytics {
-	static let sharedTracker = Manager(trackingID: "UA-42119014-6", appBundle: Bundle.main, userID: Authentication.getUsernameHash())
+	// private static let shared = Manager(trackingID: "UA-42119014-6", appBundle: Bundle.main, userID: Authentication.getUsernameHash())
 
 	static func trackView(_ viewName: String) {
-		sharedTracker.trackPageview(viewName)
+		// shared.trackPageview(viewName)
 	}
 
 	static func trackEvent(category: String, action: String, label: String?, value: String?) {
-		sharedTracker.trackEvent(category: category, action: action, label: label, value: value)
+		// shared.trackEvent(category: category, action: action, label: label, value: value)
 	}
 
 	static func trackButtonClick(_ buttonName: String) {
-		trackEvent(category: "Button", action: "Click", label: buttonName, value: nil)
+		// trackEvent(category: "Button", action: "Click", label: buttonName, value: nil)
 	}
 
 	static func trackAudioPlaybackEvent(_ actionName: String) {
-		trackEvent(category: "AudioPlayback", action: actionName, label: nil, value: nil)
+		// trackEvent(category: "AudioPlayback", action: actionName, label: nil, value: nil)
 	}
 }
